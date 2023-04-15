cc1fbb6fdb9b9930580a7e89aca29ce7f6a8dcf2
ryuujin <mirchaemanuel@gmail.com>
2023-04-15 14:13:02 +0200

    execute task command

--- app/Commands/Execute.php
+++ app/Commands/Execute.php
@@ -2,19 +2,18 @@
 
 namespace App\Commands;
 
-use App\Exceptions\MissingDotEnvFileException;
-use App\Exceptions\MissingOpenAIKeyException;
 use App\Services\AI\OpenAIService;
 use App\Traits\OpenAICommand;
-use App\Utils\CheckDotEnv;
+use Exception;
 use Illuminate\Console\Scheduling\Schedule;
-use LaravelZero\Framework\Commands\Command;
 use Illuminate\Support\Facades\Process;
+use LaravelZero\Framework\Commands\Command;
 
 
 class Execute extends Command
 {
     use OpenAICommand;
+
     /**
      * The signature of the command.
      *
@@ -40,9 +39,7 @@ class Execute extends Command
             if (!$this->checkOpenAI($openAIService)) {
                 return;
             }
-        } catch (\Exception $e) {
-            $this->error($e->getMessage());
-            return;
+        } catch (Exception $e) {
         }
 
         /*
@@ -62,24 +59,60 @@ class Execute extends Command
         /*
          * ask the user if it is ok to execute the task
          */
-        $confirm = $this->confirm('Do you want the AI to execute the task?');
-        if (!$confirm) {
-            $this->error('The task was not executed.');
-            return;
-        }
+//        $confirm = $this->confirm('Do you want the AI to execute the task?');
+//        if (!$confirm) {
+//            $this->error('The task was not executed.');
+//            return;
+//        }
 
         /*
          * This prompt has been suggested by Vuppi @IllegalStudio
          */
-        $aiRole = <<<TXT
-you are now an ai that takes a prompt and interacts with a computer sending commands and getting back the output. that means that from now everything you get from this side is the output of a command. the only exception is if the input starts with 
-prompt: 
-In that case i'm asking you what to do. You have to answer only with new commands to be launched and only one command at a time. Then you wait for the output and, based on that you send the subsequent command. Just answer Ok to this message to ackwnoloedge the task.
+        $prompt = <<<TXT
+You are now an ai that takes a prompt and interacts with a computer sending commands and
+getting back the output. That means that from now everything you get from this side is
+the output of a command. The only exception is if the input starts with "TASK".
+In that case i'm asking you what to do. You have to answer only with new commands
+to be launched and only one command at a time. Then you wait for the output and, based
+on that you send the subsequent command. You MUST NOT add any other information, only the command to be
+executed and wait for the output. If to accomplish the task you need more commands, you must send them
+one at a time, wait the output and then send the next one. For no reason you must not send more than one or
+write anything else of the command to be executed. Sometimes you must guess what operating system or
+what capabilities the cli has. If the prompt than doesnt start with "TASK:", it means it is the
+output of the previous command.
+If there is an error, for no reason you should write anything else of the new command to be executed.
+When the task is presumably complete, you must send the command "exit" to terminate the execution, and the prompt will end.
+Just answer Ok to this message to acknowledge the task.
 TXT;
 
-        $openAIService->buildClient();
+        $conversation = [
+            ['role' => 'system', 'content' => 'assistant'],
+            ['role'=> 'user', 'content' => $prompt],
+            ['role'=>'assistant', 'content' => 'OK'],
+        ];
+        $conversation[] = ['role' => 'user', 'content' => 'TASK: ' . $task];
 
+        $client = $openAIService->getClientIstance();
 
+        $exit = false;
+        while (!$exit) {
+            $response = $client->chat()->create([
+                'model' => 'gpt-4',
+                'messages' => $conversation
+            ]);
+            //retrieve the command to execute
+            $command = $response->toArray()['choices'][0]['message']['content'];
+            if($command === 'exit'){
+                $this->info('The task has been completed.');
+                $exit = true;
+            }
+            $this->warn('EXECUTE: ' . $command);
+            $processResult = Process::run($command);
+            $this->info('OUTPUT: ' . $processResult->output());
+
+            //add the command to the conversation
+            $conversation[] = ['role' => 'assistant', 'content' => $processResult->output()];
+        }
         return;
     }
 
@@ -88,7 +121,7 @@ TXT;
      *
      * @param OpenAIService $openAIService
      * @return bool
-     * @throws \Exception unable to create .env file
+     * @throws Exception unable to create .env file
      */
 
     /**
